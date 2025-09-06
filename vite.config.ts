798ce0e6911d69faf938e3ff5381d0833aa16e9a
Bashir Danjuma <bdanjuma@webmd.net>
2025-09-06 12:40:37 +0100

    Update Vite configuration to include Tailwind CSS plugin and adjust path resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [vue()],
+  base: '/finances/',
+  plugins: [tailwindcss(), vue()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src')
+      '@': path.resolve(__dirname, 'src')
     }
   }
 })
