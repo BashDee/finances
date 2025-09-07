441b2efc2d58cb6329f45e2bbf8ec8bc2f19f702
Bashir Danjuma <bdanjuma@webmd.net>
2025-09-07 05:13:13 +0100

    Update Vite configuration to set base path to root

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/finances/',
+  base: '/',
   plugins: [tailwindcss(), vue()],
   resolve: {
     alias: {
